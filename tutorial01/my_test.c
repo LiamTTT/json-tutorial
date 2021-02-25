a29d90207d97fe9bfa41386975d25f311e0ac4cb
LiamTTT <liam.liusibo@gmail.com>
2021-02-25 21:15:55 +0800

    finish tutorial01.

--- tutorial01/my_test.c
+++ tutorial01/my_test.c
@@ -6,7 +6,11 @@
 static int main_ret = 0;
 static int test_count = 0;
 static int test_pass = 0;
-
+/* 
+使用宏以保证能够正确的指出出现错误的位置，若使用函数或者内联函数，那么每次错误提示
+的行号将会是指向相同的函数内部，没有提示的作用。effective cpp里推荐做法是尽量不要
+使用宏，因为容易出现难发现的错误，但这里是单元测试设计需要，使用宏将更方便
+ */
 #define EXPECT_EQ_BASE(equality, expect, actual, format) \
     do {\
         test_count++;\
@@ -22,9 +26,24 @@ static int test_pass = 0;
 
 static void test_parse_null() {
     my_value v;
-    v.type = MY_FALSE;
-    EXPECT_EQ_INT(MY_PARSE_OK, my_parse(&v, "null"));
+    v.type = MY_NULL;  /* 这里是传入一个默认值为MY_NULL，在经过my_parse后变成对应值 */
+    EXPECT_EQ_INT(MY_PARSE_OK, my_parse(&v, "null "));
     EXPECT_EQ_INT(MY_NULL, my_get_type(&v));
+}
+
+/* 加入true和false的单元测试 */
+static void test_parse_true() {
+    my_value v;
+    v.type = MY_NULL;
+    EXPECT_EQ_INT(MY_PARSE_OK, my_parse(&v, "true"));
+    EXPECT_EQ_INT(MY_TRUE, my_get_type(&v));
+}
+
+static void test_parse_false() {
+    my_value v;
+    v.type = MY_NULL;
+    EXPECT_EQ_INT(MY_PARSE_OK, my_parse(&v, "false"));
+    EXPECT_EQ_INT(MY_FALSE, my_get_type(&v));
 }
 
 static void test_parse_expect_value() {
@@ -59,6 +78,8 @@ static void test_parse_root_not_singular() {
 
 static void test_parse() {
     test_parse_null();
+    test_parse_true();
+    test_parse_false();
     test_parse_expect_value();
     test_parse_invalid_value();
     test_parse_root_not_singular();
--- tutorial01/myjson.c
+++ tutorial01/myjson.c
@@ -18,11 +18,31 @@ static void my_parse_whitespace(my_context* c) {
 
 /* null  = "null" */
 static int my_parse_null(my_context* c, my_value* v) {
-    EXPECT(c, 'n');
+    EXPECT(c, 'n'); /* 这一步用于保证首字母的正确 */
     if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
         return MY_PARSE_INVALID_VALUE;
     c->json += 3;
     v->type = MY_NULL;
+    return MY_PARSE_OK;
+}
+
+/* true = "true" */
+static int my_parse_true(my_context* c, my_value* v) {
+    EXPECT(c, 't');
+    if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
+        return MY_PARSE_INVALID_VALUE;
+    c->json += 3;
+    v->type = MY_TRUE;
+    return MY_PARSE_OK;
+}
+
+/* false = "false" */
+static int my_parse_false(my_context* c, my_value* v) {
+    EXPECT(c, 'f');
+    if (c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' || c->json[3] != 'e')
+        return MY_PARSE_INVALID_VALUE;
+    c->json += 4;
+    v->type = MY_FALSE;
     return MY_PARSE_OK;
 }
 
@@ -31,18 +51,30 @@ static int my_parse_null(my_context* c, my_value* v) {
 static int my_parse_value(my_context* c, my_value* v) {
     switch (*c->json) {
         case 'n':  return my_parse_null(c, v);
+        case 't':  return my_parse_true(c, v);
+        case 'f':  return my_parse_false(c, v);
         case '\0': return MY_PARSE_EXPECT_VALUE;
         default:   return MY_PARSE_INVALID_VALUE;
     }
 }
 
+/* 提示：这里应该是 JSON-text = ws value ws */
+/* 以下实现没处理最后的 ws 和 LEPT_PARSE_ROOT_NOT_SINGULAR */
 int my_parse(my_value* v, const char* json) {
-    my_context c;
-    assert(v != NULL);
+    my_context c;  /* 创建传递内容的结构体 */
+    assert(v != NULL);  /* 保证指针不为空 */
     c.json = json;
     v->type = MY_NULL;
-    my_parse_whitespace(&c);
-    return my_parse_value(&c, v);
+    my_parse_whitespace(&c);  /* 放在这里用于略去头部ws */
+    /* return my_parse_value(&c, v); */
+    /* 添加处理最后的ws和LEPT_PARSE_ROOT_NOT_SINGULAR */
+    int ret;
+    if ((ret = my_parse_value(&c, v)) == MY_PARSE_OK) { /* 先保证有第一个单词且正确了才能继续 */
+        my_parse_whitespace(&c);  /* 老规矩先清零 */
+        if (*c.json != '\0')
+            ret = MY_PARSE_ROOT_NOT_SINGULAR;
+    }
+    return ret;
 }
 
 my_type my_get_type(const my_value* v) {
