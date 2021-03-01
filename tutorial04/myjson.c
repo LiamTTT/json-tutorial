406492f0a243f6d9e0ebd4fe08d74aff71d02b5a
LiamTTT <liam.liusibo@gmail.com>
2021-03-01 20:46:40 +0800

    add utf-8 parse functions

--- tutorial04/myjson.c
+++ tutorial04/myjson.c
@@ -18,6 +18,7 @@
 #define ISDIGIT1TO9(ch)     ((ch) >= '1' && (ch) <= '9')
 /* 调用函数返回void指针后强制转换为char指针取地址中的值并将其赋值为ch */
 #define PUTC(c, ch)         do { *(char*)my_context_push(c, sizeof(char)) = (ch); } while(0)
+#define STRING_ERROR(ret)   do {c->top = head; return ret;} while(0)
 
 typedef struct {
     const char* json;
@@ -96,8 +97,44 @@ static int my_parse_number(my_context* c, my_value* v) {
     return MY_PARSE_OK;
 }
 
+static const char* my_parse_hex4(const char* p, unsigned* u) {
+    int i;
+    *u = 0;  /* init 0 */
+    for (i=0; i<4; ++i) {
+        char ch = *p++;
+        *u <<= 4;  /* 十六进制 左移2^4 左移四位为进位 */
+        if (ch>='0' && ch <= '9') *u |= ch - '0';
+        else if (ch>='A' && ch <= 'F') *u |= ch - 'A' + 10;
+        else if (ch>='a' && ch <= 'f') *u |= ch - 'a' + 10;
+        else return NULL;
+    }
+    return p;
+}
+
+static void my_encode_utf8(my_context* c, unsigned u){
+    if (u <= 0x7F)
+        PUTC(c, u & 0xFF);
+    else if (u <= 0x7FF) {
+        PUTC(c, 0xC0 | ((u >> 6) & 0xFF));
+        PUTC(c, 0x80 | ( u       & 0x3F));
+    }
+    else if (u <= 0xFFFF) {
+        PUTC(c, 0xE0 | ((u >> 12) & 0xFF));
+        PUTC(c, 0x80 | ((u >>  6) & 0x3F));
+        PUTC(c, 0x80 | ( u        & 0x3F));
+    }
+    else {
+        assert(u <= 0x10FFFF);
+        PUTC(c, 0xF0 | ((u >> 18) & 0xFF));
+        PUTC(c, 0x80 | ((u >> 12) & 0x3F));
+        PUTC(c, 0x80 | ((u >>  6) & 0x3F));
+        PUTC(c, 0x80 | ( u        & 0x3F));
+    }
+}
+
 static int my_parse_string(my_context* c, my_value* v){
     size_t head = c->top, len;
+    unsigned u, u2;
     const char* p;
     EXPECT(c, '\"');  /* the first char must be a " */
     p = c->json;
@@ -120,10 +157,29 @@ static int my_parse_string(my_context* c, my_value* v){
             case 'f': PUTC(c, '\f'); break;
             case 'n': PUTC(c, '\n'); break;
             case 'r': PUTC(c, '\r'); break;            
-            case 't': PUTC(c, '\t'); break;            
+            case 't': PUTC(c, '\t'); break;
+            case 'u':
+                if (!(p = my_parse_hex4(p, &u)))
+                    STRING_ERROR(MY_PARSE_INVALID_UNICODE_HEX);
+                if (u >= 0xD800 && u <= 0xDBFF) {
+                    /* 代理对处理 
+                    如果在0xD800-0xDBFF之间就认为是高代理项，开始解析后面的转义字符
+                    */
+                    if (*p++ != '\\')
+                        STRING_ERROR(MY_PARSE_INVALID_UNICODE_SURROGATE);
+                    if (*p++ != 'u')
+                        STRING_ERROR(MY_PARSE_INVALID_UNICODE_SURROGATE);
+                    if (!(p = my_parse_hex4(p, &u2)))
+                        STRING_ERROR(MY_PARSE_INVALID_UNICODE_HEX);
+                    if (u2 < 0xDC00 || u2 > 0xDFFF)
+                        STRING_ERROR(MY_PARSE_INVALID_UNICODE_SURROGATE);
+                    /* 左移6个 */
+                    u = (((u - 0xD800) << 10) | (u2 - 0xDC00)) + 0x10000;
+                }
+                my_encode_utf8(c, u);
+                break;
             default:
-                c->top = head;
-                return MY_PARSE_INVALID_STRING_ESCAPE;
+                STRING_ERROR(MY_PARSE_INVALID_STRING_ESCAPE);
             }
             break;
         case '\0':
